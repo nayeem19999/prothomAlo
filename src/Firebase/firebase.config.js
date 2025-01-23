00b3e1da16a1a9665e58f103bc93cf1f8a13d54b
eshikhon222 <mern2.eshikhon@gmail.com>
2025-01-23 22:36:02 +0600

    hide all firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,14 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDRWqpv8K4GaEHRDtQGD62_K1jMviPwh3E",
-  authDomain: "news-portal-afdb2.firebaseapp.com",
-  projectId: "news-portal-afdb2",
-  storageBucket: "news-portal-afdb2.firebasestorage.app",
-  messagingSenderId: "538668852828",
-  appId: "1:538668852828:web:3a2e0e4d3cca1643f22899",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
